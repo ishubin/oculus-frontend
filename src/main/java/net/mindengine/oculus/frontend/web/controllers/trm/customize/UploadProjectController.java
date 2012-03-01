ea8bb9c0e812ba72a3eb53c5baffe0988b015a3b
Ivan Shubin <ivan.ishubin@gmail.com>
2012-03-01 17:46:22 +0100

    Added license headers

--- src/main/java/net/mindengine/oculus/frontend/web/controllers/trm/customize/UploadProjectController.java
+++ src/main/java/net/mindengine/oculus/frontend/web/controllers/trm/customize/UploadProjectController.java
@@ -14,7 +14,7 @@
 * GNU General Public License for more details.
 * 
 * You should have received a copy of the GNU General Public License
-* along with Oculus Experior.  If not, see <http://www.gnu.org/licenses/>.
+* along with Oculus Frontend.  If not, see <http://www.gnu.org/licenses/>.
 ******************************************************************************/
 package net.mindengine.oculus.frontend.web.controllers.trm.customize;
 
